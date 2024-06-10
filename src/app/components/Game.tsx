8856bd900f72655486b205da14968a9519f0fb91
Tim <62808178+timothymaxwellsummers@users.noreply.github.com>
2024-06-10 11:18:19 +0200

    Added games

---
+++ src/app/components/Game.tsx
@@ -0,0 +1,42 @@
+'use client';
+import React, { useState, useMemo } from 'react';
+
+const Game: React.FC = () => {
+    const [selectedGame, setSelectedGame] = useState('https://cdn.htmlgames.com/NinjaBreakout/');
+
+    const games = useMemo(() => [
+        { name: 'Neon Nibblet', url: 'https://cdn.htmlgames.com/NeonNibblet/' },
+        { name: 'Ninja Breakout', url: 'https://cdn.htmlgames.com/NinjaBreakout/' },
+        { name: 'Upsidedown', url: 'https://cdn.htmlgames.com/UpsideDown/' },
+        { name: 'Coloring for Kids', url: 'https://cdn.htmlgames.com/ColoringForKids/' }
+    ], []);
+
+    return (
+        <div className="flex flex-col items-center justify-center bg-gray-100 min-h-screen">
+            <h1 className="text-2xl font-bold mb-4">Choose a Game</h1>
+            <div className="flex space-x-4 mb-8">
+                {games.map((game) => (
+                    <button
+                        key={game.name}
+                        onClick={() => setSelectedGame(game.url)}
+                        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition duration-300"
+                    >
+                        {game.name}
+                    </button>
+                ))}
+            </div>
+            {selectedGame && (
+                <div className="w-full flex justify-center">
+                    <iframe
+                        src={selectedGame}
+                        title="ADHD Game"
+                        className="border-2 border-gray-300"
+                        style={{ width: '800px', height: '600px' }}
+                    ></iframe>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default React.memo(Game);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import DashboardComponent from "./components/DashboardComponent";
+import Game from "./components/Game";
 
 export default function Home() {
   return (
     <main className="">
       <h1 className="text-2xl font-bold my-4 ml-4">🚧 AffectiveADHD Testing 🚧</h1>
+      <Game />
       <DashboardComponent />
     </main>
   );
