21ba7ee6a1b96d5908c88bdab20052fb3a4898b7
blackSwan566 <57305281+blackSwan566@users.noreply.github.com>
2024-06-12 13:49:45 +0200

    Feedback Popupcard implemented
    I implemented a popup card

--- src/app/components/Game.tsx
+++ src/app/components/Game.tsx
@@ -1,8 +1,10 @@
 'use client';
 import React, { useState, useMemo } from 'react';
+import {Popup} from './Feedback'
 
 const Game: React.FC = () => {
     const [selectedGame, setSelectedGame] = useState('https://cdn.htmlgames.com/NinjaBreakout/');
+    const [showPopup, setShowPopup] = useState(false); // Popup state
 
     const games = useMemo(() => [
         { name: 'Neon Nibblet', url: 'https://cdn.htmlgames.com/NeonNibblet/' },
@@ -11,19 +13,33 @@ const Game: React.FC = () => {
         { name: 'Coloring for Kids', url: 'https://cdn.htmlgames.com/ColoringForKids/' }
     ], []);
 
+    const togglePopup = () => setShowPopup(!showPopup); 
+
+
+
     return (
         <div className="flex flex-col items-center justify-center bg-gray-100 min-h-screen">
-            <h1 className="text-2xl font-bold mb-4">Choose a Game</h1>
+            <h1 className="text-2xl font-bold mb-4">Choose a Game
+            </h1>
             <div className="flex space-x-4 mb-8">
                 {games.map((game) => (
                     <button
                         key={game.name}
-                        onClick={() => setSelectedGame(game.url)}
+                        onClick={() => {
+                            
+                            togglePopup(); 
+                            setSelectedGame(game.url);
+                            
+                        }}
+
                         className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition duration-300"
-                    >
+                    >   
                         {game.name}
                     </button>
                 ))}
+
+                <Popup showPopup={showPopup} togglePopup={togglePopup} />
+                
             </div>
             {selectedGame && (
                 <div className="w-full flex justify-center">
