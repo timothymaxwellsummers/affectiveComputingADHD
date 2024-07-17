1ca7d4569f10fd51582834b8c0b439df4440a427
Tim <62808178+timothymaxwellsummers@users.noreply.github.com>
2024-07-17 14:58:24 +0200

    Updated Logo + Translations

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -8,7 +8,7 @@ const DashboardPage: React.FC = () => {
   return (
     <div className="">
       <h1 className="text-2xl font-bold mt-2 ml-4 text-[rgb(0,14,128)] ">
-        Parent Dashboard
+        Eltern Dashboard
       </h1>
       <div className="flex flex-col md:flex-row p-12 space-y-8 md:space-y-0 md:space-x-8 pt-20 ">
         <div className="flex-1 space-y-4 rounded-xl shadow-xl bg-[rgb(255,255,255)]">
